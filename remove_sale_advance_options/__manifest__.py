c95794ed75ff21ab1e79f007d873bf1e178bec7a
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-09-11 07:06:15 +0300

    Add files via upload

---
+++ remove_sale_advance_options/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Remove Sale Order Advance Payment Options',
+    'version': '6.1.0',
+    'category': 'Sales',
+    "author": 'Zero Systems',
+    "company": 'Zero for Information Systems',
+    "website": "https://www.erpzero.com",
+    "email": "sales@erpzero.com",
+    "sequence": 0,
+    'license': 'OPL-1',
+    'live_test_url': 'https://youtu.be/vw_2EQAv9nU',
+    'summary': 'Sale Order Advance Payment Options By company setting',
+    "description": 'Sale Order Advance Payment Options By company setting',
+    'depends': ['sale','sale_management'],
+    'data': [
+        'views/view.xml',
+
+    ],
+    "price": 25.00,
+    "currency": 'EUR',
+    'installable': True,
+    'auto_install': False,
+    "application": True,
+    'images': ['static/description/no_sale_adv.png'],
+    'pre_init_check_vers': 'pre_init_check_vers',
+}
+
