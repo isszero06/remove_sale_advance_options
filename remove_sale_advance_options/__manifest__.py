c0e491751f0e6de8a12432ca26a0fdc323c5ca5c
Zero Systems <isszero06@gmail.com>
2024-01-14 18:59:13 +0200

    Update __manifest__.py

--- remove_sale_advance_options/__manifest__.py
+++ remove_sale_advance_options/__manifest__.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Zero For Information Systems (<www.erpzero.com>)
+# Copyright(c): 2016-Zero For Information Systems
+# All Rights Reserved.
+#Zerosystems #odoo #erp
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
 {
     'name': 'Remove Sale Order Advance Payment Options',
     'version': '6.1.0',
